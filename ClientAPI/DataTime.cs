1dba31ccc8d7ebaa4da1ce309bfa3cf3fd29346a
Artur <arturfortsizi@gmail.com>
2024-02-13 21:33:39 +0200

    Черновой функционал

--- ClientAPI/DataTime.cs
+++ ClientAPI/DataTime.cs
@@ -6,9 +6,11 @@ namespace Weather_Forecast.ClientAPI
 {
     class DataTime
     {
+        //Создание переменных
         private System.Timers.Timer timer;
         private System.Windows.Controls.Label timeLabel;
 
+        //Создание и запуск таймера в конструкторе
         public DataTime(System.Windows.Controls.Label label)
         {
             timeLabel = label;
@@ -17,6 +19,7 @@ namespace Weather_Forecast.ClientAPI
             timer.Start();
         }
 
+        //Вывод времени в приложении 
         private void DataShow(object sender, ElapsedEventArgs e)
         {
             timeLabel.Dispatcher.Invoke(() => timeLabel.Content = DateTime.Now.ToString("HH:m:s"));
--- ClientAPI/Weather_API.cs
+++ ClientAPI/Weather_API.cs
@@ -6,15 +6,18 @@ namespace Weather_Forecast.ClientAPI
 {
     class Weather_API : MainWindow
     {
+        //Создание переменных
         private const string API_KEY = "a0d36a11bde668359c9fd3eaec26be68";
         private string city;
 
+        //Создание конструктора и запись города в него
         public Weather_API(string city)
         {
             this.city = city;
             TreatmentData();
         }
 
+        //Работа с сылками !(Отловить ошибки с городом)!
         public async Task<string> GetWeather()
         {
             HttpClient client = new HttpClient();
@@ -22,6 +25,7 @@ namespace Weather_Forecast.ClientAPI
             return await client.GetStringAsync(url);
         } 
 
+        //Работа с json и вывод результата
         private async void TreatmentData()
         {
             string data = await GetWeather();
