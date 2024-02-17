07f9bb2e90391a0c23ecf2a634a41ce77de8b683
Artur <arturfortsizi@gmail.com>
2024-02-17 09:11:39 +0200

    Добавлено визуализацию

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Drawing;
+using System.Windows;
+using System.Windows.Media;
 using Weather_Forecast.ClientAPI;
 
 namespace Weather_Forecast
@@ -16,9 +18,12 @@ namespace Weather_Forecast
             timers = new DataTime(TimeLabel);
         }
 
+        //System.Windows.Media.Color color = System.Windows.Media.Color.FromRgb(1,1,1);
+
         //Отслеживание нажатия на кнопку !(Проверять TextBox на налчие символов)!
         public void GetWeatherBtn_Click(object sender, RoutedEventArgs e)
         {
+            //BackGround.Background = new SolidColorBrush(color);
             Weather_API weather = new Weather_API(CountryTextBox.Text);
         }
     }
