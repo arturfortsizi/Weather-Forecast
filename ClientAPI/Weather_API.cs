c7b51a284b82d2812175e0ac07f53dc3da38fbdd
Artur <arturfortsizi@gmail.com>
2024-02-22 15:28:14 +0200

    Добавлены параметры рогоды

--- ClientAPI/Weather_API.cs
+++ ClientAPI/Weather_API.cs
@@ -23,16 +23,28 @@ namespace Weather_Forecast.ClientAPI
             HttpClient client = new HttpClient();
             string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric";
             return await client.GetStringAsync(url);
-        } 
+        }
 
         //Работа с json и вывод результата
+        //Добавить вывод скорости ветра, порывы, мин. и макс. температуру
+        //Влажность и давление
+
+        /*json["main"]["temp_min"].ToString();
+         *json["main"]["temp_max"].ToString();
+         *json["wind"]["speed"];- скорость
+         *json["wind"]["gust"];- порыв  
+         *json["main"]["humidity"];- влажность
+         *
+         */
         private async void TreatmentData()
         {
             string data = await GetWeather();
 
             var json = JObject.Parse(data);
-            string temperature = json["main"]["temp"].ToString();
-            MessageBox.Show(temperature);
+            //string temperature = json["main"]["temp"].ToString();
+            MessageBox.Show($"Температура-{json["main"]["temp"]}, мин-{json["main"]["temp_min"]}, макс-{json["main"]["temp_max"]}");
+            MessageBox.Show($"Влажность: {json["main"]["humidity"]}, Скорость: {json["wind"]["speed"]}");
+            MessageBox.Show($"Порыв:{json["wind"]["gust"]}");
         }
 
     }
