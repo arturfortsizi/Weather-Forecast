a0b88bd336bfc1f0b00f63058b928f44b10a0827
Artur <arturfortsizi@gmail.com>
2024-03-22 13:59:20 +0200

    Добавил кнопку темы //Рассписать темы, добавит иконку кнопки

---
+++ JSON_Save.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Windows;
+
+namespace Weather_Forecast
+{
+    class JSON_Save
+    {
+        string pathFile = "data.json";
+
+        //Сохранить файл
+        public void JSONSave(bool theme_bool)
+        {
+            string jsonTheme = JsonConvert.SerializeObject(theme_bool);
+            File.WriteAllText(pathFile, jsonTheme);
+            MessageBox.Show($"{jsonTheme},{pathFile}");
+        }
+
+        //Загрузить файл
+        public bool JSONLoad()
+        {
+            string jsonSave = File.ReadAllText(pathFile);
+            return JsonConvert.DeserializeObject<bool>(jsonSave);
+        }
+    }
+}
